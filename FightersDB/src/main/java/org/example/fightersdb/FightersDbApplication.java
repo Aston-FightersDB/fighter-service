989cfe07d91d59ff5457487e40e28b423b9ec1e6
Yuri Mishchenko <30956226+Yumintin@users.noreply.github.com>
2025-03-25 15:36:32 +0600

    Создание пустого проекта

---
+++ FightersDB/src/main/java/org/example/fightersdb/FightersDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.fightersdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightersDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FightersDbApplication.class, args);
+    }
+
+}
